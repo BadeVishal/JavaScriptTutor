44b6979ebb89d7e4592e486f178651e15d6a7f84
Vishal Bade <106652783+Vishal-Bade@users.noreply.github.com>
2023-07-27 13:59:20 +0000

    datatype conversion

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-console.log('Vishal Bade');
+// console.log('Vishal Bade');
 
 let a = 10;
-let b = 29;
+let b = 30;
 
 let c = (a + b)/2;
-console.log(c);
+// console.log(c);
+
+// *********** Datatype Conversion ************
+let d = String(202);
+console.log(typeof d, d);
+
+let e  = String(true);
+console.log(typeof e , e );
+
+let f = Number('12');
+console.log(typeof (f), f);
+
+let g = Number('12cbk'); //Not A Number
+console.log(typeof (g), g);
+
+let h = Boolean('vis');
+console.log(typeof (h), h);
+
+let i = Boolean('');
+console.log(typeof (i), i);
+
+let j = Boolean(122);
+console.log(typeof (j), j);
+
+let k = Boolean();
+console.log(typeof (k), k);
